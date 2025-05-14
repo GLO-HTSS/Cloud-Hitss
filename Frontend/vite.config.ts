6fce1f4528f78a01a081c6180530b96c5336acfa
Katan <fer.mtz.guz@gmail.com>
2025-05-14 16:55:48 -0500

    re conf front

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,29 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@hitss": path.resolve(__dirname, "../Backend/src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-    },
-  },
-  server: {
-    port: 5173,
-    host: true,
-    fs: {
-      allow: [".."],
-    },
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 });
